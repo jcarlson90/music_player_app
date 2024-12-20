50f2c5b47b11e19b94a88175a1ca4c790ee3a218
jcarlson <jake.carlson90@gmail.com>
2024-12-20 14:52:01 -0800

    small addition to discover.js

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -6,22 +6,20 @@ export const shazamCoreApi = createApi({
     baseUrl: "https://shazam-core.p.rapidapi.com",
     prepareHeaders: (headers) => {
       headers.set(
-        // "x-rapidapi-key",
-        // "711599b2e1msh1b66f3aaa0b58e8p1661a8jsnc138f843cb7d"
+        "x-rapidapi-key",
+        "711599b2e1msh1b66f3aaa0b58e8p1661a8jsnc138f843cb7d"
       );
-      headers.set("x-rapidapi-host", "shazam-core.p.rapidapi.com");
+      headers.set(
+        "x-rapidapi-host", 
+        "shazam-core.p.rapidapi.com"
+      );
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({
-      query: () => "/v1/charts/country?country_code=US",
+      query: () => "/v1/charts/world?country_code=US",
     }),
-    getArtistDetails: builder.query({
-      query: (artistId) => "/v1/artists/details?artist_id=${artistId}",
-    }),
-    getSongsByCountry: builder.query({ 
-      query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
   }),
 });
 

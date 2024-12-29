02d2429cb9dc03954a69a7aa1b5b9d49f2d76768
jcarlson <jake.carlson90@gmail.com>
2024-12-29 14:14:27 -0800

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,61 @@
 import { useState } from 'react';
-//import { navLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { HiOutlineMenu } from 'react-icons/hi';
 import { RiCloseLine } from 'react-icons/ri';
 
 import { logo } from '../assets';
 import { links } from '../assets/constants';
 
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
 
 const Sidebar = () => {
-  const [mobileMenu, setMobileMenuOpen] = useState (false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className= "md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-    </div>
-  )
+    <>
+      {/* Sidebar for Desktop */}
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+
+      {/* Sidebar for Mobile */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine
+            onClick={() => setMobileMenuOpen(false)}
+            className="w-6 h-6 text-white mr-2"
+          />
+        ) : (
+          <HiOutlineMenu
+            onClick={() => setMobileMenuOpen(true)}
+            className="w-6 h-6 text-white mr-2"
+          />
+        )}
+      </div>
+
+      {mobileMenuOpen && (
+        <div className="absolute top-0 h-screen w-full bg-[#191624] flex flex-col z-10">
+          <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+          <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Sidebar;

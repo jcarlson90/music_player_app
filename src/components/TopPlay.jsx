a23a9c787ec59c70d4061be34b3af2f610ca9c5c
jcarlson <jake.carlson90@gmail.com>
2025-01-03 23:09:41 -0800

    start topPlay

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,23 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useref } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Freemode} from 'swiper';
+
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => 
+  state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+};
 
 export default TopPlay;

e9324d3e540998a1203037c082969757cbd9e4ae
jcarlson <jake.carlson90@gmail.com>
2025-01-07 14:54:42 -0800

    console errors

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -17,12 +17,12 @@ const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handle
     <div className="flex-1 flex flex-row justify-between items-center">
       <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
       <div className="flex-1 flex flex-col justify-center mx-3">
-        <Link to={`/songs/${song?.key}`}>{song?.attributes}
+        <Link to={`/songs/${song.key}`}>
           <p className="text-xl font-bold text-white">
             {song?.title}
           </p>
         </Link>
-        <Link to={`/songs/${song?.key}`}>{song?.attributes}
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
           <p className="text-base text-gray-300 mt-1">
             {song?.subtitle}
           </p>
@@ -81,10 +81,7 @@ const TopPlay = () => {
               handlePauseClick={handlePauseClick}
               handlePlayClick={() => handlePlayClick(song, i)}
             />
-            
-            
           ))}
-          
         </div>
       </div>
 
@@ -94,7 +91,6 @@ const TopPlay = () => {
           <Link to="/top-artists">
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
-          
         </div>
 
         <Swiper
@@ -122,6 +118,5 @@ const TopPlay = () => {
     </div>
   );
 };
-
 
 export default TopPlay;
